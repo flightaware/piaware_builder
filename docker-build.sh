8f255a3832ca8f5586b594f35ac6e19a84100acb
Guy Taylor <thebigguy.co.uk@gmail.com>
2018-07-09 17:46:04 +0100

    Output files from Docker using the current user

--- docker-build.sh
+++ docker-build.sh
@@ -9,21 +9,28 @@ set -o pipefail         # Use last non-zero exit code in a pipeline
 DISTRIBUTION=${1}
 
 DOCKER_IMAGE_NANE="piaware-builder:${DISTRIBUTION}"
-DOCKER_CONTINAER_NANE='piaware-builder'
+DOCKER_CONTINAER_NANE="piaware-builder-${DISTRIBUTION}"
 
 # build builder image
 docker build --tag "${DOCKER_IMAGE_NANE}" --file "Dockerfile-${DISTRIBUTION}" .
 
+# Setup the build env
 mkdir -p "package-${DISTRIBUTION}"
 docker run --rm -it --name "${DOCKER_CONTINAER_NANE}" \
+    -u="${UID}:$(id -g ${USER})" -v '/etc/group:/etc/group:ro' -v '/etc/passwd:/etc/passwd:ro' \
     -v "${PWD}:/build:ro" \
     -v "${PWD}/package-${DISTRIBUTION}:/build/package-${DISTRIBUTION}:rw" \
     --workdir="/build" "${DOCKER_IMAGE_NANE}" \
     ./sensible-build.sh "${DISTRIBUTION}"
 
+# build the deb packages
 mkdir -p "debs-${DISTRIBUTION}"
 docker run --rm -it --name "${DOCKER_CONTINAER_NANE}" \
+    -u="${UID}:$(id -g ${USER})" -v '/etc/group:/etc/group:ro' -v '/etc/passwd:/etc/passwd:ro' \
     -v "${PWD}/debs-${DISTRIBUTION}:/build:rw" \
     -v "${PWD}/package-${DISTRIBUTION}:/build/package-${DISTRIBUTION}:rw" \
     --workdir="/build/package-${DISTRIBUTION}" \
     "${DOCKER_IMAGE_NANE}" dpkg-buildpackage -b
+
+rm -rf "package-${DISTRIBUTION}"
+rm -rf "debs-${DISTRIBUTION}/package-${DISTRIBUTION}"
