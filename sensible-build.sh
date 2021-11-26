dd78615810d0b686c5a8e92c4fbd9b2b36407e95
Oliver Jowett <oliver.jowett@flightaware.com>
2021-11-26 14:57:39 +0800

    pip didn't work out, switch back to setup.py. download/install cxfreeze dependencies. archive download cleanups.

--- sensible-build.sh
+++ sensible-build.sh
@@ -79,6 +79,7 @@ else
 fi
 
 mkdir -p $OUTDIR
+mkdir -p $OUTDIR/archives
 
 clone_or_update https://github.com/flightaware/piaware.git origin/dev $OUTDIR/piaware
 
@@ -90,27 +91,33 @@ clone_or_update https://github.com/mutability/mlat-client.git v0.2.11 $OUTDIR/ml
 
 clone_or_update https://github.com/flightaware/dump978.git origin/dev $OUTDIR/dump978
 
+fetch_archive() {
+    name=$1
+    url=$2
+    hash=$3
+
+    if [ ! -f $OUTDIR/$name.tar.gz ]; then
+        echo "Fetching $name .."
+        wget -nv -O $OUTDIR/archives/$name.tar.gz.unchecked $url
+        echo "$hash $OUTDIR/archives/$name.tar.gz.unchecked" | sha256sum -c -
+        mv $name.tar.gz.unchecked $name.tar.gz
+    fi
+
+    tar -C $OUTDIR $OUTDIR/archives/$name.tar.gz $name/
+}
+
 # get a copy of cxfreeze and patch it for building on Debian
 case $debdist in
     stretch)
         # stretch has Python 3.5; cx-freeze 6.3 is the latest version supporting Python 3.5
-        if [ ! -d $OUTDIR/cx_Freeze-6.3 ]
-        then
-            echo "Retrieving cxfreeze 6.3"
-            wget -nv -O $OUTDIR/cx_Freeze-6.3.tar.gz https://github.com/anthony-tuininga/cx_Freeze/archive/6.3.tar.gz
-            echo "ac6212e44e072869de5153dd81e5d1c369b2ef73e75ed58cbb81ab59b4eaf6e1 $OUTDIR/cx_Freeze-6.3.tar.gz" | sha256sum -c -
-            tar -C $OUTDIR -zxf $OUTDIR/cx_Freeze-6.3.tar.gz
-        fi
+        fetch_archive cx_Freeze-6.3 https://github.com/anthony-tuininga/cx_Freeze/archive/6.3.tar.gz ac6212e44e072869de5153dd81e5d1c369b2ef73e75ed58cbb81ab59b4eaf6e1
         ;;
     buster|bullseye)
         # Buster has Python 3.7, Bullseye has Python 3.9; both are supported by the latest cx-freeze at the time of writing (6.8)
-        if [ ! -d $OUTDIR/cx_Freeze-6.8.3 ]
-        then
-            echo "Retrieving cxfreeze 6.8.3"
-            wget -nv -O $OUTDIR/cx_Freeze-6.8.3.tar.gz https://github.com/anthony-tuininga/cx_Freeze/archive/6.8.3.tar.gz
-            echo "d39c59fdfc82106dfe1e5dce09f2537a3cc82dc8295024f40f639d94193979c3 $OUTDIR/cx_Freeze-6.8.3.tar.gz" | sha256sum -c -
-            tar -C $OUTDIR -zxf $OUTDIR/cx_Freeze-6.8.3.tar.gz
-        fi
+        fetch_archive more-itertools-8.12.0 https://github.com/more-itertools/more-itertools/archive/refs/tags/v8.12.0.tar.gz b708a64c95508d4f1fef137acbe3b482b6a95d0cd1b54400c50534cb0ae05923
+        fetch_archive zipp-3.6.0 https://github.com/jaraco/zipp/archive/refs/tags/v3.6.0.tar.gz fd1af7ebc49a73dd0cd35f31d44a28022566e5114bdef64129fab0d9dc524486
+        fetch_archive importlib_metadata-4.8.2 https://github.com/python/importlib_metadata/archive/refs/tags/v4.8.2.tar.gz d490678f0f37b0508571fda77e37542898acfa20c8716cff349d357ecb781dcd
+        fetch_archive cx_Freeze-6.8.3 https://github.com/anthony-tuininga/cx_Freeze/archive/6.8.3.tar.gz d39c59fdfc82106dfe1e5dce09f2537a3cc82dc8295024f40f639d94193979c3
         ;;
 esac
 
