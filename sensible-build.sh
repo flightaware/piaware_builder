bdd75ebe7ba4e0d7b43caf0e3985d6e1efa66182
Oliver Jowett <oliver.jowett@flightaware.com>
2026-04-06 11:15:37 +0800

    restore noble build in sensible-build

--- sensible-build.sh
+++ sensible-build.sh
@@ -65,6 +65,11 @@ case $dist in
     targetdist=jammy
     extraversion="~ubuntu2204+"
     ;;
+  noble)
+    debdist=trixie
+    targetdist=noble
+    extraversion="~ubuntu2404+"
+    ;;
   *)
     echo "unknown build distribution $1" >&2
     usage
